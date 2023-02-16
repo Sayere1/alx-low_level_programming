5662eb2859217024d497f88aa6459d29ee744f7c
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-02-16 06:44:22 -0800

    unux

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: 1 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n"", stdout);
-	return (1);
+	write(2 "and that piece of art is useful\" - Dora korpar, 2015-10-19\n"", 59);
+	return (1)
 }
