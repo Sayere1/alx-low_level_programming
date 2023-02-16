f73761e4d828e1b6259cb3bf893b5491d44ee1a0
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-02-16 06:29:18 -0800

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Write a c programm without using the printf and put functions
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that peice of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+	fput("and that peice of art is useful\" - Dora korpar, 2015-10-19\n"", stdout);
 	return (1);
 }
