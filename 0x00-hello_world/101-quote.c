75c5cbce0bc5844325f5bb355aabee7fd5f5ed0c
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-02-16 11:14:16 -0800

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fput("and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
