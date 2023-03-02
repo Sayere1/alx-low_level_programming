72092bdfa47b5d80890708d272753a1154f84e70
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-03-01 16:26:07 -0800

    strcat with 2 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,7 @@
  * This function appends the src string to the dest string
  * overwriting the terminating null byte (\0) at the end of dest,
  * and then adds a terminating null byte
-returns a pointer to the resulting string dest
+ * returns a pointer to the resulting string dest
  * @dest: string to append to
  * @src: string to add
  *
