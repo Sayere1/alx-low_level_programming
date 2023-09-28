b2bcc627ad5ac686540890dc6e18f24b43df5da3
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-09-28 14:47:15 -0700

    1.1 Write a function that prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - Write a function that print binary representation of a number
+ * no usage of malloc
+ * no usage of % or / operations
+ * @n: decimal number to print as binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int shifts;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (; shifts >= 0; shifts--)
+		;
+
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+	{
+		if ((n >> shifts) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
