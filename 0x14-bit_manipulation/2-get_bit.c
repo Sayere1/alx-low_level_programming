7619d9d584d36e67eb83250f7173e94baaf5d817
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-09-28 16:18:06 -0700

    2.10 Write a function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,6 +4,7 @@
  * get_bit - Write a function that returns the value of a bit at a given index
  * @n: number to evaluate
  * @index: starting from 0, of the bit we want to get
+ *
  * Return: The Value of bit at index, or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
