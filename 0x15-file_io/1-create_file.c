710217f465d4e42cd347b591f03653cfc075056e
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-10-02 10:13:48 -0700

    This program create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,14 +9,14 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int x, y, len = 2;
+	int x, y, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 2; text_content[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
