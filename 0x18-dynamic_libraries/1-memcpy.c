639b1ddfe744dcc45129605bf59a285b0d8f8805
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-11-13 07:50:57 -0800

    the dynamics

--- 0x18-dynamic_libraries/1-memcpy.c
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _memcpy - func that copies memory to a location
- * @dest: char pointer
- * @src: char memory location
- * @n: bytes from src to dest
- * Return: dest.
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0;  < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
--- 0x18-dynamic_libraries/3-islower.c
+++ 0x18-dynamic_libraries/3-islower.c
@@ -1,4 +1,4 @@
-#iinclude "main.h"
+#include "main.h"
 
 /**
  * _islower - func for lowercase character
