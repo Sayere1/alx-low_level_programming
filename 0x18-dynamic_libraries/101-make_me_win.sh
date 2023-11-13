ec257e0785fe979f2697444bf82f13d8157da89b
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-11-13 08:32:04 -0800

    Code injection: Win the Giga Millions

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/gigalib.so "https://raw.githubusercontent.com/Sayere1/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so"
-export LD_PRELOAD=/tmp/gigalib.so
+wget -P /tmp https://github.com/Sayere1/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
